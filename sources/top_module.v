49ed247777cc2b7f003270b93c43666106950634
SimonPrato <simip.prato@gmail.com>
2026-01-05 10:08:41 +0100

    addigned finish signal to avoid unconnected

--- sources/top_module.v
+++ sources/top_module.v
@@ -30,7 +30,7 @@ module top_module(
     
     // Reset synchronization for LFSR/MISR
     wire lfsr_misr_reset;
-    assign lfsr_misr_reset = reset | controller_init;
+    assign lfsr_misr_reset = reset | controller_init | controller_finish;
     
     // BIST Controller
     controller controller_inst (
